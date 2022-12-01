0d31c64a194fe061bfc21c936b77cf28e226a7d7
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-12-01 07:22:51 +0100

    part 2

--- day-01/main.ts
+++ day-01/main.ts
@@ -14,21 +14,23 @@ async function readLinesFromFile() {
 
 const lines = await readLinesFromFile();
 
-let r = lines.reduce(
-  (acc, line) => {
-    if (line === "") {
-      acc.push(0);
+let r = lines
+  .reduce(
+    (acc, line) => {
+      if (line === "") {
+        acc.push(0);
+
+        return acc;
+      }
+
+      acc[acc.length - 1] += Number(line);
 
       return acc;
-    }
+    },
+    [0]
+  )
+  .sort((a, b) => a - b);
 
-    acc[acc.length - 1] += Number(line);
+let answer = r.pop() + r.pop() + r.pop();
 
-    return acc;
-  },
-  [0]
-);
-
-r = Math.max.apply(null, r);
-
-console.log("result", r);
+console.log("result", answer);
