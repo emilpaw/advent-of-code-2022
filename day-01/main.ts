472c6f3e3b085f64ba9df72c73790c16de8d542a
Emil Pawlowski <39118431+emilpaw@users.noreply.github.com>
2022-12-01 07:17:33 +0100

    part1

---
+++ day-01/main.ts
@@ -0,0 +1,34 @@
+import { readLines } from "https://deno.land/std@0.166.0/io/buffer.ts";
+import * as m from "https://deno.land/std@0.166.0/collections/mod.ts";
+
+async function readLinesFromFile() {
+  const file = await Deno.open("./input.txt");
+
+  const list = [];
+  for await (const l of readLines(file)) {
+    list.push(l);
+  }
+
+  return list;
+}
+
+const lines = await readLinesFromFile();
+
+let r = lines.reduce(
+  (acc, line) => {
+    if (line === "") {
+      acc.push(0);
+
+      return acc;
+    }
+
+    acc[acc.length - 1] += Number(line);
+
+    return acc;
+  },
+  [0]
+);
+
+r = Math.max.apply(null, r);
+
+console.log("result", r);
